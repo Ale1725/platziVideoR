b8fc7c5f85fa87df8fceefef1e3c5c3371bf797e
dtime <david171612@gmail.com>
2020-08-13 14:53:09 -0400

    ACTUALIZACION DEL COMPONENTE

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ import "../assets/styles/components/Search.scss";
 const Search = () => (
   <section className="main">
     <h2 className="main__title">What would you like to see today?</h2>
-    <input type="text" className="input" placeholder="Buscar..." />
+    <input type="text" className="input" placeholder="Search..." />
   </section>
 );
 
